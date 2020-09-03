928c185681bcfc406a2adc4744999fbdde7cf58d
karan <karansagar30@gmail.com>
2020-09-03 15:22:41 +0530

    commit v2

--- testback/index.js
+++ testback/index.js
@@ -4,4 +4,7 @@ const port =3000;
 
 app.get('/', (req,res)=> {res.send("hello world")} );
 
+const admin = (req,res)=> {res.send(" admin dash board")}
+
+app.get('/admin',admin);
 app.listen(port, ()=>{console.log('connection set reaDY to go')});
